336067fd6ca28e2fc74fbc4f2b7a2aa1289dc543
sadik_malik <sadik.malik@vietpay.com>
2024-03-20 22:42:58 +0530

    feat(@piplup/use-local-storage): added @piplup/use-event-listener to external

--- packages/use-local-storage/vite.config.ts
+++ packages/use-local-storage/vite.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', "@piplup/use-event-listener"],
     },
   },
 });
